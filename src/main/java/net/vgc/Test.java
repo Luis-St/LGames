0ff1c0b8c007debb611582e548e9aadd3fbfe96c
Luis <76595940+Luis-st@users.noreply.github.com>
2022-03-31 20:52:58 +0200

    add test run

---
+++ src/main/java/net/vgc/Test.java
@@ -0,0 +1,17 @@
+package net.vgc;
+
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+public class Test extends Application {
+
+	public static void main(String[] args) {
+		launch(args);
+	}
+
+	@Override
+	public void start(Stage stage) throws Exception {
+		
+	}
+
+}
