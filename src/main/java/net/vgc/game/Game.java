cf9000492c449825610c067c11bbb9ab8ff8cdbc
Luis <76595940+Luis-st@users.noreply.github.com>
2022-05-01 21:12:21 +0200

    move TTTGame#removePlayer and TTTGame#stopGame into it's super class

--- src/main/java/net/vgc/game/Game.java
+++ src/main/java/net/vgc/game/Game.java
@@ -1,6 +1,7 @@
 package net.vgc.game;
 
 import java.util.List;
+import java.util.Objects;
 
 import javax.annotation.Nullable;
 
@@ -8,6 +9,10 @@ import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
 import net.vgc.game.action.ActionHandler;
+import net.vgc.network.packet.client.ExitGamePacket;
+import net.vgc.network.packet.client.StopGamePacket;
+import net.vgc.server.Server;
+import net.vgc.server.dedicated.DedicatedServer;
 import net.vgc.server.player.ServerPlayer;
 
 public interface Game {
@@ -48,8 +53,36 @@ public interface Game {
 		}
 	}
 	
-	boolean removePlayer(ServerPlayer player);
+	default boolean removePlayer(ServerPlayer player) {
+		if (this.getPlayers().remove(player)) {
+			player.connection.send(new ExitGamePacket());
+			if (Objects.equals(this.getCurrentPlayer(), player)) {
+				this.nextPlayer();
+			}
+			if (!this.getType().enoughPlayersToPlay(this.getPlayers())) {
+				this.stopGame();
+			}
+			return true;
+		}
+		return false;
+	}
 	
-	void stopGame();
+	default void stopGame() {
+		DedicatedServer server = Server.getInstance().getServer();
+		for (ServerPlayer player : this.getPlayers()) {
+			player.connection.send(new StopGamePacket());
+		}
+		for (ServerPlayer player : server.getPlayerList().getPlayers()) {
+			if (this.getPlayers().contains(player) && player.isPlaying()) {
+				player.setPlaying(false);
+			} else if (player.isPlaying()) {
+				player.setPlaying(false);
+				LOGGER.info("Correcting the playing value of player {} to false, since it was not correctly reset", player.getGameProfile().getName());
+			}
+		}
+		this.getPlayers().clear();
+		server.setGame(null);
+		LOGGER.info("Game {} was successfully stopped", this.getType().getName());
+	}
 	
 }
--- src/main/java/net/vgc/game/ttt/TTTGame.java
+++ src/main/java/net/vgc/game/ttt/TTTGame.java
@@ -1,16 +1,11 @@
 package net.vgc.game.ttt;
 
 import java.util.List;
-import java.util.Objects;
 
 import net.vgc.game.Game;
 import net.vgc.game.GameType;
 import net.vgc.game.GameTypes;
 import net.vgc.game.action.ActionHandler;
-import net.vgc.network.packet.client.ExitGamePacket;
-import net.vgc.network.packet.client.StopGamePacket;
-import net.vgc.server.Server;
-import net.vgc.server.dedicated.DedicatedServer;
 import net.vgc.server.player.ServerPlayer;
 
 public class TTTGame implements Game {
@@ -45,38 +40,6 @@ public class TTTGame implements Game {
 	@Override
 	public void setCurrentPlayer(ServerPlayer currentPlayer) {
 		this.currentPlayer = currentPlayer;
-	}
-	
-	@Override
-	public boolean removePlayer(ServerPlayer player) {
-		this.players.remove(player);
-		player.connection.send(new ExitGamePacket());
-		if (Objects.equals(this.currentPlayer, player)) {
-			this.nextPlayer();
-		}
-		if (!this.getType().enoughPlayersToPlay(this.players)) {
-			this.stopGame();
-		}
-		return false;
-	}
-	
-	@Override
-	public void stopGame() {
-		DedicatedServer server = Server.getInstance().getServer();
-		for (ServerPlayer player : this.players) {
-			player.connection.send(new StopGamePacket());
-		}
-		for (ServerPlayer player : server.getPlayerList().getPlayers()) {
-			if (this.players.contains(player) && player.isPlaying()) {
-				player.setPlaying(false);
-			} else if (player.isPlaying()) {
-				player.setPlaying(false);
-				LOGGER.info("Correcting the playing value of player {} to false, since it was not correctly reset", player.getGameProfile().getName());
-			}
-		}
-		this.players.clear();
-		server.setGame(null);
-		LOGGER.info("Game {} was successfully stopped", this.getType().getName());
 	}
 	
 }
